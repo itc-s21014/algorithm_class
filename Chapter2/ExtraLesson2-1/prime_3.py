e333522ecdfc4952d02bca271dabd8ee582df64f
s21014 <s21014@std.it-coollege.ac.jp>
2023-05-16 12:10:14 +0900

    課題完了

--- Chapter2/ExtraLesson2-1/prime_3.py
+++ Chapter2/ExtraLesson2-1/prime_3.py
@@ -1,11 +1,11 @@
-p = [True]*100
+p = [True]*82
 p[0] = False
 p[1] = False
 n = 2
 
 def hyou():
     s = ""
-    for i in range(100):
+    for i in range(82):
         if p[i] == True:
             s += "{:2d}, ".format(i)
         else:
@@ -16,16 +16,16 @@ def hyou():
 
 def furui():
     global n
-    for i in range(n+n, 100, n):
+    for i in range(n+n, 82, n):
         p[i] = False
     print(n, "の倍数を篩い落としました")
     hyou()
-    while n<100:
+    while n<82:
         n = n + 1
         if p[n] == True:
             break
 
 hyou()
-while n<10:
+while n<9:
     input("Enterキーを押してください")
     furui()
